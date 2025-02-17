eb612e240fc8bcc1bf79f95d269486cd894df6a2
Andres Perez <andresperez0401@gmail.com>
2025-02-17 16:14:03 -0400

    Semaforo funcional, cuando se hace click en algun color, este se ilumina

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -4,15 +4,26 @@ import { useEffect, useState } from "react";
 
  export function Semaforo() {
 
-    
-
+    //Estado que almacena el color del semaforo, inicia sin color
+    const [color, setColor] = useState("");
 
 
     return  <div className="cajaSemaforo">
-                <div className="redLight"></div>
-                <div className="yellowLight"></div>
-                <div className="greenLight"></div>;
+                <div
+                 className={color == "red" ? "redLightSelected" : "redLight"}
+                 id="red" 
+                 onClick={() => setColor("red")}
+                 ></div>
+
+                <div 
+                className={color == "yellow" ? "yellowLightSelected" : "yellowLight"}
+                id="yellow"
+                onClick={() => setColor("yellow")}
+                ></div>
+
+                <div className={color == "green" ? "greenLightSelected" : "greenLight"}
+                id="green" 
+                onClick={() => setColor("green")}
+                ></div>
             </div>
-
-
  }
